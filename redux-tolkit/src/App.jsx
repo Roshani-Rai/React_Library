bddfdd999ebe89178f8106ab65e5c1f7813538d0
Roshani-Rai <roshaniray2626@gmail.com>
2025-07-02 14:01:48 +0530

    reduxToolkit

---
+++ redux-tolkit/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import AddTodo from './AddTodo'
+import Todos from './Todos'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='text-4xl font-bold text-gray-700'>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
