a7a9cd10ed34c548ea64c513925aa8e043443215
Roshani-Rai <roshaniray2626@gmail.com>
2025-07-27 21:47:10 +0530

    completed

--- Note-store/src/Paste.jsx
+++ Note-store/src/Paste.jsx
@@ -27,18 +27,18 @@ function Paste(){
           <div className='text-3xl text-white font-bold px-8'>{paste.title}</div>
           <div className='text-2xl text-gray-200 font-semibold px-8'>{paste.content}</div>
              </div>
-          <div className='flex flex-row gap-4 justify-end mx-10 -mt-10'>
-            <button className='rounded-md px-4 mx-7 w-16  text-white py-2 text-xl bg-slate-800 font-bold'>
+          <div className='flex flex-row gap-2 justify-end mx-4 -mt-6'>
+            <button className='rounded-md px-4 text-white py-2 text-[15px] bg-slate-800 font-bold'>
                < Link to={`/paste/${paste?.id}`}>
-               <i className="fa-solid fa-eye" style={{color:'white'}}>View  </i></Link> </button>
-            <button className='rounded-md px-2 text-white py-2 text-xl bg-slate-800 font-bold' > <i className="fa-solid fa-pen" style={{color:'whites'}}><Link to={`/?pasteId=${paste?.id}`}> Edit </Link></i></button>
-            <button className='rounded-md px-2 text-white py-2 text-xl bg-slate-800 font-bold' onClick={()=>dispatch(remove(paste))}> <i className="fa-solid fa-xmark" style={{color:'white'}}>Delete</i></button>
-            <button className='rounded-md px-2 text-white py-2 text-xl bg-slate-800 font-bold' 
+               <i className="fa-solid fa-eye" style={{color:'white'}}></i>View </Link> </button>
+            <button className='rounded-lg px-2 text-white py-2 text-[15px] bg-slate-800 font-bold' > <i className="fa-solid fa-pen" style={{color:'whites'}}></i><Link to={`/?pasteId=${paste?.id}`}> Edit </Link></button>
+            <button className='rounded-md px-2 text-white py-2 text-[15px] bg-slate-800 font-bold' onClick={()=>dispatch(remove(paste))}> <i className="fa-solid fa-xmark" style={{color:'white'}}></i>Delete</button>
+            <button className='rounded-md px-2 text-white py-2 text-[15px] bg-slate-800 font-bold' 
             onClick={()=>{navigator.clipboard.writeText(paste.content);
               toast.success("Copied Successfully")
             }}>
-              <i className="fa-solid fa-copy" style={{color:'white'}}> Copy </i> </button>
-            <button className='rounded-md px-2 text-white py-2 text-xl bg-slate-800 font-bold' 
+              <i className="fa-solid fa-copy" style={{color:'white'}}> </i>Copy  </button>
+            <button className='rounded-md px-2 text-white py-2 text-[15px] bg-slate-800 font-bold' 
               onClick={()=>{
                 if(navigator.share){
                   navigator.share({
@@ -48,7 +48,7 @@ function Paste(){
                   })
                 }
               }}>
-              <i className="fa-solid fa-share" style={{color:'white'}}> Share </i></button>
+              <i className="fa-solid fa-share" style={{color:'white'}}></i> Share </button>
           </div>
         </div>
       )
